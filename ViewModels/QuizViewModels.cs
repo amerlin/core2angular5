acc1c300e0862b52b4b5331f494f7be73535f216
Andrea Merlin <merlin.andrea@gmail.com>
2018-03-26 19:39:37 +0200

    add QuizViewModel

---
+++ ViewModels/QuizViewModels.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace core2angular5.ViewModels
+{
+    [JsonObject(MemberSerialization.OptOut)]
+    public class QuizViewModels
+    {
+        public QuizViewModels()
+        {
+
+        }
+
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Text { get; set; }
+        public string Notes { get; set; }
+
+        [DefaultValue(0)]
+        public int Type { get; set; }
+
+        [DefaultValue(0)]
+        public int Flags { get; set; }
+        public string UserId { get; set; }
+
+        [JsonIgnore]
+        public int ViewCount { get; set; }
+
+        public DateTime CreateDate { get; set; }
+        public DateTime LastModifiedDate { get; set; }
+    }
+}
